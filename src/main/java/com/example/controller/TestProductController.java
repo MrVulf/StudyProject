a0e64decf0e185f692e4c491de1755698afb23b2
mrVulf <vulfcorp@mail.ru>
2019-12-03 12:09:21 +0300

    fix problem with using pages

--- src/main/java/com/example/controller/TestProductController.java
+++ src/main/java/com/example/controller/TestProductController.java
@@ -2,29 +2,36 @@ package com.example.controller;
 
 import com.example.model.Product;
 import com.example.service.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
-@RestController
-@RequestMapping(value = "/test")
+
+@Controller
+@RequestMapping("/test")
 public class TestProductController {
     @Autowired
     private ProductService productService;
 
+    private static final Logger LOG = LoggerFactory.getLogger(TestProductController.class);
 
-    @GetMapping("/products")
+    @RequestMapping(value = "/products", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
     public List<Product> getAllCountries() {
+
+        LOG.info("==== TEST TEST TEST =====");
         return productService.getAll();
     }
 
-    @GetMapping("/products&{productId}")
-    public Optional<Product> getProductById(@RequestParam Long id){
-        return productService.getProductById(id);
+    @RequestMapping(value = "/products/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public Optional<Product> getProductById(@PathVariable String id){
+        return productService.getProductById(Long.parseLong(id));
     }
 }
