74acefc91c236bb17f6d635fb2497db4dfd8deda
mrVulf <vulfcorp@mail.ru>
2019-12-10 13:26:06 +0300

    DELETE ShopCart

--- src/main/java/com/example/repository/ShopRepository.java
+++ src/main/java/com/example/repository/ShopRepository.java
@@ -1,8 +1,7 @@
 package com.example.repository;
 
-import com.example.model.ShopCart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ShopRepository extends JpaRepository<ShopCart, Integer> {
+public interface ShopRepository{ //extends JpaRepository<ShopCart, Integer> {
 
 }
--- src/main/java/com/example/service/ShopCartService.java
+++ src/main/java/com/example/service/ShopCartService.java
@@ -1,15 +1,15 @@
 package com.example.service;
 
 import com.example.model.Product;
-import com.example.model.ShopCart;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Collection;
 
 public interface ShopCartService{
+    /*
     void createShopCart(ShopCart cart);
     void updateShopCart(String id, ShopCart cart);
     void deleteShopCart(String id);
     void setProduct(Product product);
     Collection<Product> getProducts();
+     */
 }
